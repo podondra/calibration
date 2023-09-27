adf60f553bbf1a9a514bedcf263a3819a2b7f789
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2023-09-27 13:23:56 +0200

    Add figures

--- calibration/plot.py
+++ calibration/plot.py
@@ -9,8 +9,8 @@ STEPS = 100
 
 def density(ax, pdf, start=START, end=-START, steps=STEPS, **kwargs):
     x = torch.linspace(start, end, steps)
-    ax.plot(x, pdf(x.unsqueeze(-1)), **kwargs)
+    return ax.plot(x, pdf(x.unsqueeze(-1)), **kwargs)[0]
 
 
 def pit_hist(ax, x, n_bins=pit.BINS, **kwargs):
-    ax.stairs(x, torch.linspace(0, 1, n_bins + 1), **kwargs)
+    return ax.stairs(x, torch.linspace(0, 1, n_bins + 1), **kwargs)
