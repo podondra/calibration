4f782e08962926591e77ee18fc98b1fd09c6d973
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2023-09-29 09:01:00 +0200

    Create figure of training process

--- calibration/plot.py
+++ calibration/plot.py
@@ -12,5 +12,10 @@ def density(ax, pdf, start=START, end=-START, steps=STEPS, **kwargs):
     return ax.plot(x, pdf(x.unsqueeze(-1)), **kwargs)[0]
 
 
+def cumulative_density(ax, cdf, start=START, end=-START, steps=STEPS, **kwargs):
+    x = torch.linspace(start, end, steps)
+    return ax.plot(x, cdf(x.unsqueeze(-1)), **kwargs)[0]
+
+
 def pit_hist(ax, x, n_bins=pit.BINS, **kwargs):
     return ax.stairs(x, torch.linspace(0, 1, n_bins + 1), **kwargs)
